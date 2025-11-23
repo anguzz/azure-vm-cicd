bd29cb57277dd929bf40fad68ccd593944ed8f16
Angel <santoyox714@gmail.com>
2025-11-23 11:39:57 -0800

    Remove backend state to test deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,6 @@
 terraform {
   required_version = ">= 1.4.0"
 
-  backend "azurerm" {}
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -119,23 +117,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 
-# Storage Account for Terraform state
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_id.sa.hex}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 
-  allow_blob_public_access = false
-}
-
-resource "random_id" "sa" {
-  byte_length = 4
-}
-
-# Container for tf state
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name = azurerm_storage_account.tfstate.name
-}
