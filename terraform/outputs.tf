3351926b0909df77b9077b493aaf7f675a62d684
Angel <santoyox714@gmail.com>
2025-11-23 14:12:15 -0800

    Correct jsondecode type error for azapi private key

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,8 @@ output "public_ip_address" {
 }
 
 output "private_key_pem" {
-  # This value extracts the privateKey from the JSON output of the azapi action
-  value     = jsondecode(azapi_resource_action.ssh_public_key_gen.output).privateKey
-  # CRITICAL: This hides the key from normal Terraform log output in CI/CD
+  value     = azapi_resource_action.ssh_public_key_gen.output.privateKey
+  
+  # This hides the key from normal Terraform log output in CI/CD
   sensitive = true 
 }
