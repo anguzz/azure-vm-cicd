58b953aa0ff3865345c91d16802f4594a269854f
Angel <santoyox714@gmail.com>
2025-11-23 12:36:14 -0800

    Resolve Public IP limit and OIDC authentication failure

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,8 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static" # Standard SKU often requires Static allocation
+  sku                 = "Standard" 
 }
 
 # Create Network Security Group and rule
